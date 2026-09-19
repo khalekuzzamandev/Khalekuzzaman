08376773a44bdbce1e29ea49f2d0c11c6b864944
muradvcv <muradvcv@gmail.com>
2026-09-19 10:34:33 +0600

    get project from server

--- src/lib/actions/project.js
+++ src/lib/actions/project.js
@@ -2,11 +2,17 @@
 const baseUrl = process.env.BACKEND_URL
 
 // post project
-export const createProject=async(newProject)=>{
-  const res = await fetch(`${baseUrl}/api/project/post`,{
-    method:'POST',
-    headers:{'content-type':'application/json'},
-    body:JSON.stringify(newProject)
+export const createProject = async (newProject) => {
+  const res = await fetch(`${baseUrl}/api/project/post`, {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify(newProject)
   })
+  return res.json()
+}
+
+// get projects
+export const getProject = async () => {
+  const res = await fetch(`${baseUrl}/api/project/get`)
   return res.json()
 }
--- src/lib/test.js
+++ src/lib/test.js
@@ -1,10 +1,1 @@
-function matchCharacter(str,char){
-  let count =0
-  for(let c of str){
-    if(c==char){
-      count ++;
-    }
-  }
-  return count;
-}
-console.log(matchCharacter('ddd','h'));
+const number =
